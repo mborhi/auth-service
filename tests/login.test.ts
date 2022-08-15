8db39e261f0cd73c7cedd2ec449c447996c47582
Marcell Borhi <mborhi@iu.edu>
2022-08-15 19:51:07 -0400

    outline tests for login and token endpoints and utils

---
+++ tests/login.test.ts
@@ -0,0 +1,19 @@
+describe("Login", () => {
+
+    it("correctly stores given token with session id", () => {
+
+    });
+
+    it("correctly makes a request to the Spotify Web API token endpoint", () => {
+
+    });
+
+    it("correctly handles a token response from the Spotify Web API", () => {
+
+    });
+
+    it("correctly handles any errors from the Spotify Web API", () => {
+
+    });
+
+});
---
+++ tests/token.test.ts
@@ -0,0 +1,17 @@
+describe("Token", () => {
+
+    it("correctly retrieves token from Redis cache", () => {
+
+    });
+
+    it("correctly makes a request to the Spotify Web API if the token is expired", () => {
+
+    });
+
+    it("correctly updates the cache with a new token", () => {
+
+    });
+
+});
+
+// TODO: add LFU for cache
