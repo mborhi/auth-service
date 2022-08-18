9f04e32dbd344226e37e8d595f91fe014267ee32
Marcell Borhi <mborhi@iu.edu>
2022-08-18 02:12:31 -0400

    token retrieval endpoint and utilities

--- src/routes/token.ts
+++ src/routes/token.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import { getAccessToken } from '../utils/token-util';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("Hellow from the /token endpoint!");
+// retrieves the token from cache
+router.get('/', async (req, res) => {
+    const session_id = req.headers.session_id as string; // validated at API gateway
+    try {
+        const access_token = await getAccessToken(session_id);
+        res.status(200).json(access_token);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            "error": {
+                "status": 500,
+                "message": "Internal server error"
+            }
+        });
+    }
+
 });
 
 export default router;
---
+++ src/utils/token-util.ts
@@ -0,0 +1,65 @@
+import { SpotifyToken } from "../../interfaces";
+import Redis from "ioredis";
+import endpointsConfig from "../../endpoints.config";
+import fetch from "node-fetch";
+import { dataIsError, responseIsError } from "./fetch-utils";
+
+// TODO: make util file to configure this for prod/dev env
+// connect to the Redis client
+const redisClient = new Redis({
+    host: 'localhost',
+    port: 6379
+});
+
+const baseURL = endpointsConfig.SpotifyAPIBaseURL;
+
+/**
+ * Retrieves the access token of the user with the given session id
+ * @param session_id the session id of the user requesting the access token
+ * @returns the access token
+ */
+export const getAccessToken = async (session_id: string) => {
+    const token: SpotifyToken = JSON.parse(await redisClient.get(session_id));
+    if (Date.now() >= token.expires_at) {
+        // make request to get new token
+        const newToken = await refreshToken(token);
+        // validate that new token is not error
+        if (dataIsError(newToken)) return newToken;
+        // update redis cache
+        redisClient.set(session_id, JSON.stringify(newToken));
+        // return the access token
+        return newToken.access_token;
+    }
+    return token.access_token;
+};
+
+/**
+ * Refreshes the given token by making a request to the Spotify Web API
+ * @param token the token to refresh
+ * @returns a new token
+ */
+const refreshToken = async (token: SpotifyToken): Promise<SpotifyToken> => {
+    const url = baseURL + '/token';
+    const client_id = endpointsConfig.ClientId;
+    const client_secret = endpointsConfig.ClientSecret;
+    const bodyParams = {
+        grant_type: 'refresh_token',
+        refresh_token: token.refresh_token
+    }
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
+        },
+        body: JSON.stringify(bodyParams)
+    });
+    if (responseIsError(response)) return await response.json();
+    try {
+        const data = await response.json();
+        const newToken = { ...data, refresh_token: token.refresh_token }
+        return newToken;
+    } catch (error) {
+        throw error;
+    }
+}
