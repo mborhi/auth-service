f5eee2896aede9dbbc31540ab3ea67a1910851d8
Marcell Borhi <mborhi@iu.edu>
2022-08-20 11:11:24 -0400

    Revert "add spotify api types"
    This reverts commit 41c443d6c44a19d81aaaac0d5e755070a6f405c2.

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="./node_modules/@types/spotify-api/index.d.ts" />
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,4 +1,3 @@
-// authentication
 export interface SpotifyToken {
     "access_token": string,
     "token_type": string,
@@ -6,26 +5,9 @@ export interface SpotifyToken {
     "expires_at": number,
     "refresh_token": string
 }
-
-// API call
 export interface ErrorObject {
     "error": {
         "status": number
         "message": string
     }
-}
-
-// retrieval
-export interface TrackData {
-    name: string
-    previewURL: string
-    trackURI: string
-    trackNum: number
-    trackAlbumImage: string
-}
-
-// playback 
-export interface PlaybackStateData {
-    progress: number
-    availableActions: SpotifyApi.ActionsObject
 }
